eb4b3ee322c5d142e7259443274e4c6556947873
prameshwor <prameshworchaudhary14@gmail.com>
2024-01-24 22:29:47 +0545

    number and string

---
+++ python/number__string.py
@@ -0,0 +1,15 @@
+#integer number 
+2
+3
+-9
+-8
+#floating point number 
+2.0
+1.5
+1.3
+#string (it start with alphabet or underscore(_))
+'i am using python'
+"i am learning python"
+"abc@123"
+
+
