71b96f89061ae12f90e36aa0c724852f5ab3cbba
prameshwor <prameshworchaudhary14@gmail.com>
2024-01-24 22:43:34 +0545

    new

---
+++ python/variables.py
@@ -0,0 +1,14 @@
+#variable declaration
+a=20
+b=30
+c=20.5
+name="python"
+#to display a value use: print()
+print(a) #output: 20
+print(b) #output: 30
+print(c) #output: 20.5
+print(name) #output: python
+print("the value of a is",a) ##output: the value of a is 20
+print("the value of a is ",a,"and the value of b is ",b) ##output: the value of a is 20 and the value of b is 30
+result=a+b
+print(a,"+",b,"=",result) #output: 20 + 30 = 50
