2a1bec767fc6739ab663abec5c0ce937e0f60f0d
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-03 20:14:05 +0545

    condition statement

---
+++ python/conditional_statement.py
@@ -0,0 +1,19 @@
+# two types of operator
+#1. relational operator
+# <,>,<=,>=,==,!=
+a=10
+b=20
+print(a>b) #output:false
+print(b>a) #output:true
+print(a<=b) #output:true
+print(a>=b)#output:false
+print(a==b)#output:false
+print(a!=b) #output:true
+ # 2. logical operator
+ # and (&&) , or (||) , not (!)
+x=10
+y=20
+z=30
+print((x>y)and(y>z)) #output: false
+print((x<y)or(y<z)) #output: true
+ 
