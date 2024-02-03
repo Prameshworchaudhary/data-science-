d17cd1b90dfb6a3f3fd1fdaead70a051d7521b39
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-03 20:51:46 +0545

    condition statement

--- python/conditional_statement.py
+++ python/conditional_statement.py
@@ -16,10 +16,22 @@ y=20
 z=30
 print((x>y)and(y>z)) #output: false
 print((x<y)or(y<z)) #output: true
+
+#if else condition
 number=20
 if(number%2==0):
     print(number,"is a even.")
 else:
     print(number,"is a odd.")
+
+#if elseif elseif .... else
+num1=30
+num2=40
+if(num1<num2):
+    print(num1,"is less than",num2)
+elif(num1>num2):
+    print(num1,"is greater than",num2)
+else:
+    print(num1,"is equal to",num2)
     
     
