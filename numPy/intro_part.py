8bfb357550075cddebab59ce2ae084a416677cb7
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-09 19:02:36 +0545

    intro_part

---
+++ numPy/intro_part.py
@@ -0,0 +1,9 @@
+#numpy is a python libaries.
+# it is used for working with array.
+#numpy is short for numerical python.
+#The array object in NumPy is called ndarray, it provides a lot of supporting functions that make working with ndarray very easy.
+import numpy as np
+arr=np.array([1,2,3,4,5])
+print(arr)
+print(type(arr))   
+print(np.__version__) # to see the version
