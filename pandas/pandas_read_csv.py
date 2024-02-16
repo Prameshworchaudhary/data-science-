2b740aa9d37ead348ef47a65dfa5786952bb5650
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-16 23:19:10 +0545

    read csv file

---
+++ pandas/pandas_read_csv.py
@@ -0,0 +1,15 @@
+#A simple way to store big data sets is to use CSV files (comma separated files).
+#CSV files contains plain text and is a well know format that can be read by everyone including Pandas.
+
+#in example download this -> Download data.csv.
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+print(df.to_string()) 
+
+#Print the DataFrame without the to_string() method:
+
+df = pd.read_csv('data.csv')
+
+print(df)
