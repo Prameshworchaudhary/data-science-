8ba04136605febd45becd2e0e8bc160d7bc91dcb
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-22 22:13:27 +0545

    machine learning intro

---
+++ machine learning/intro_part.py
@@ -0,0 +1,22 @@
+#Machine Learning is making the computer learn from studying data and statistics.
+
+#Machine Learning is a step into the direction of artificial intelligence (AI).
+
+#Machine Learning is a program that analyses data and learns to predict the outcome.
+"""Data Types
+To analyze data, it is important to know what type of data we are dealing with.
+
+We can split the data types into three main categories:
+
+a.Numerical
+b.Categorical
+c.Ordinal
+a.Numerical data are numbers, and can be split into two numerical categories:
+
+1.Discrete Data
+- counted data that are limited to integers. Example: The number of cars passing by.
+2.Continuous Data
+- measured data that can be any number. Example: The price of an item, or the size of an item
+b.Categorical data are values that cannot be measured up against each other. Example: a color value, or any yes/no values.
+
+c.Ordinal data are like categorical data, but can be measured up against each other. Example: school grades where A is better than B and so on."""
