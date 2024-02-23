283696bf96e6e66c88d3ff418216822a14c7e538
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-23 13:06:54 +0545

    scripy intro

---
+++ Scipy/scripy_intro.py
@@ -0,0 +1,13 @@
+"""SciPy is a scientific computation library that uses NumPy underneath.
+
+SciPy stands for Scientific Python.
+
+It provides more utility functions for optimization, stats and signal processing."""
+
+from scipy import constants
+
+print(constants.pi)
+
+print(constants.kilo)
+
+print(dir(constants))
