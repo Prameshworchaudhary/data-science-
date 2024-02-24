03fe5e1a3cc706cbb325d65e458b4efba0a970f4
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-24 21:41:48 +0545

    multiple  regression

---
+++ machine learning/multiple_regression.py
@@ -0,0 +1,33 @@
+# Multiple Regression
+# Multiple regression is like linear regression, but with more than one independent value, meaning that we try to predict a value based on two or more variables.
+
+import pandas
+from sklearn import linear_model
+
+df = pandas.read_csv("data.csv")
+
+X = df[['Weight', 'Volume']]
+y = df['CO2']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+#predict the CO2 emission of a car where the weight is 2300kg, and the volume is 1300cm3:
+predictedCO2 = regr.predict([[2300, 1300]])
+
+print(predictedCO2)
+
+#Coefficient
+#The coefficient is a factor that describes the relationship with an unknown variable.
+import pandas
+from sklearn import linear_model
+
+df = pandas.read_csv("data.csv")
+
+X = df[['Weight', 'Volume']]
+y = df['CO2']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+print(regr.coef_)
