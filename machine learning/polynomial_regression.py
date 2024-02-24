5dca777ec91295f210a2ede038f51469409c0bbf
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-24 21:37:32 +0545

    polynomial regression

---
+++ machine learning/polynomial_regression.py
@@ -0,0 +1,44 @@
+"""Polynomial Regression
+If your data points clearly will not fit a linear regression (a straight line through all data points), it might be ideal for polynomial regression.
+
+Polynomial regression, like linear regression, uses the relationship between the variables x and y to find the best way to draw a line through the data points."""
+
+import numpy
+import matplotlib.pyplot as plt
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+
+myline = numpy.linspace(1, 22, 100)
+
+plt.scatter(x, y)
+plt.plot(myline, mymodel(myline))
+plt.show()
+"""R-Squared
+It is important to know how well the relationship between the values of the x- and y-axis is, if there are no relationship the polynomial regression can not be used to predict anything.
+
+The relationship is measured with a value called the r-squared."""
+import numpy
+from sklearn.metrics import r2_score
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+
+print(r2_score(y, mymodel(x)))
+# Predict Future Values
+# Now we can use the information we have gathered to predict future values.
+
+import numpy
+from sklearn.metrics import r2_score
+
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+mymodel = numpy.poly1d(numpy.polyfit(x, y, 3))
+
+speed = mymodel(17)
+print(speed)
