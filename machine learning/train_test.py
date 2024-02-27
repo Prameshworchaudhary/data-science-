28bea7344defd66f123452c17e0aa5df3c949592
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-27 21:07:39 +0545

    python

---
+++ machine learning/train_test.py
@@ -0,0 +1,45 @@
+#Train/Test is a method to measure the accuracy of your model.
+
+#It is called Train/Test because you split the data set into two sets: a training set and a testing set.
+
+import numpy
+import matplotlib.pyplot as plt
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+plt.scatter(x, y)
+plt.show()
+
+#The training set should be a random selection of 80% of the original data.
+
+#The testing set should be the remaining 20%.
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+plt.scatter(train_x, train_y)
+plt.show()
+
+#Bring in the Testing Set
+import numpy
+from sklearn.metrics import r2_score
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+r2 = r2_score(test_y, mymodel(test_x))
+
+print(r2)
